899a35d1912a774e62172b356f10fd08027a73c4
Franko961 <franko961@hotmail.com>
2026-07-11 23:54:09 +0200

    Update mandante_repository.py

--- backend/repositories/mandante_repository.py
+++ backend/repositories/mandante_repository.py
@@ -7,6 +7,9 @@ class MandanteRepository:
     async def find_many(self, user_id: str) -> list:
         return await self.collection.find({"user_id": user_id}, {"_id": 0}).to_list(500)
 
+    async def find_one(self, mid: str, user_id: str):
+        return await self.collection.find_one({"id": mid, "user_id": user_id}, {"_id": 0})
+
     async def insert(self, doc: dict) -> dict:
         await self.collection.insert_one(doc)
         doc.pop("_id", None)
