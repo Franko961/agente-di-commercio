679b35b2a0aaaa30b1f507c8b462a40e2c8881d2
Franko961 <franko961@hotmail.com>
2026-07-11 14:57:14 +0200

    Create exceptions.py

---
+++ backend/core/exceptions.py
@@ -0,0 +1,23 @@
+class AppError(Exception):
+    status_code = 500
+    detail = "Errore interno"
+
+    def __init__(self, detail: str = None):
+        if detail:
+            self.detail = detail
+        super().__init__(self.detail)
+
+
+class NotFoundError(AppError):
+    status_code = 404
+    detail = "Risorsa non trovata"
+
+
+class PermissionDeniedError(AppError):
+    status_code = 403
+    detail = "Permesso negato"
+
+
+class ValidationAppError(AppError):
+    status_code = 400
+    detail = "Dati non validi"
