c3e361d9f1d52deb0d2db80525bf7a2da8dd4013
Franko961 <franko961@hotmail.com>
2026-07-12 01:10:42 +0200

    Create document_repository.py

---
+++ backend/repositories/document_repository.py
@@ -0,0 +1,38 @@
+from core.database import db
+from typing import Optional
+
+
+class DocumentRepository:
+    collection = db.documents
+
+    async def find_many(self, user_id: str) -> list:
+        return await self.collection.find(
+            {"user_id": user_id, "is_deleted": {"$ne": True}}, {"_id": 0}
+        ).sort("created_at", -1).to_list(2000)
+
+    async def find_one(self, did: str, user_id: str, include_deleted: bool = False) -> Optional[dict]:
+        query = {"id": did, "user_id": user_id}
+        if not include_deleted:
+            query["is_deleted"] = {"$ne": True}
+        return await self.collection.find_one(query, {"_id": 0})
+
+    async def insert(self, doc: dict) -> dict:
+        await self.collection.insert_one(doc)
+        doc.pop("_id", None)
+        return doc
+
+    async def update_meta(self, did: str, user_id: str, data: dict) -> bool:
+        res = await self.collection.update_one(
+            {"id": did, "user_id": user_id, "is_deleted": {"$ne": True}},
+            {"$set": data},
+        )
+        return res.matched_count > 0
+
+    async def soft_delete(self, did: str, user_id: str) -> None:
+        await self.collection.update_one(
+            {"id": did, "user_id": user_id},
+            {"$set": {"is_deleted": True}},
+        )
+
+
+document_repository = DocumentRepository()
