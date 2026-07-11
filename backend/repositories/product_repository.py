17590cd3d56338ab0f7f10477bf80798c475edf0
Franko961 <franko961@hotmail.com>
2026-07-11 20:13:00 +0200

    Update product_repository.py

--- backend/repositories/product_repository.py
+++ backend/repositories/product_repository.py
@@ -13,6 +13,7 @@ class ProductRepository:
 
     async def insert(self, doc: dict) -> dict:
         await self.collection.insert_one(doc)
+        doc.pop("_id", None)
         return doc
 
     async def update(self, pid: str, user_id: str, data: dict) -> None:
