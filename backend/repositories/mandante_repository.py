0397529a2b8b61a418ff491ff8a1f67eb1e7d904
Franko961 <franko961@hotmail.com>
2026-07-11 19:02:49 +0200

    Create mandante_repository.py

---
+++ backend/repositories/mandante_repository.py
@@ -0,0 +1,22 @@
+from core.database import db
+
+
+class MandanteRepository:
+    collection = db.mandanti
+
+    async def find_many(self, user_id: str) -> list:
+        return await self.collection.find({"user_id": user_id}, {"_id": 0}).to_list(500)
+
+    async def insert(self, doc: dict) -> dict:
+        await self.collection.insert_one(doc)
+        return doc
+
+    async def update(self, mid: str, user_id: str, data: dict) -> bool:
+        res = await self.collection.update_one({"id": mid, "user_id": user_id}, {"$set": data})
+        return res.matched_count > 0
+
+    async def delete(self, mid: str, user_id: str) -> None:
+        await self.collection.delete_one({"id": mid, "user_id": user_id})
+
+
+mandante_repository = MandanteRepository()
