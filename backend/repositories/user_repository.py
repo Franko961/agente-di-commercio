09c61712b2ee004b3353683af5eca0184f50cc67
Franko961 <franko961@hotmail.com>
2026-07-12 23:13:11 +0200

    refactor: aggiunge find_by_id/update_by_id/update_by_stripe_subscription_id a user_repository

--- backend/repositories/user_repository.py
+++ backend/repositories/user_repository.py
@@ -13,5 +13,14 @@ class UserRepository:
         doc.pop("_id", None)
         return doc
 
+    async def find_by_id(self, uid: str) -> Optional[dict]:
+        return await self.collection.find_one({"id": uid}, {"_id": 0, "password_hash": 0})
+
+    async def update_by_id(self, uid: str, data: dict) -> None:
+        await self.collection.update_one({"id": uid}, {"$set": data})
+
+    async def update_by_stripe_subscription_id(self, sub_id: str, data: dict) -> None:
+        await self.collection.update_one({"stripe_subscription_id": sub_id}, {"$set": data})
+
 
 user_repository = UserRepository()
