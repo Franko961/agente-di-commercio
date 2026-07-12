9a0f529c59684682de7b5958f35d454729dd3528
Franko961 <franko961@hotmail.com>
2026-07-12 22:44:49 +0200

    refactor: estrae email_log_repository.py (accesso db.email_logs)

---
+++ backend/repositories/email_log_repository.py
@@ -0,0 +1,18 @@
+from core.database import db
+
+
+class EmailLogRepository:
+    collection = db.email_logs
+
+    async def find_many(self, user_id: str) -> list:
+        return await self.collection.find(
+            {"user_id": user_id}, {"_id": 0}
+        ).sort("created_at", -1).to_list(200)
+
+    async def insert(self, doc: dict) -> dict:
+        await self.collection.insert_one(doc)
+        doc.pop("_id", None)
+        return doc
+
+
+email_log_repository = EmailLogRepository()
