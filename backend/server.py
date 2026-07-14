bd9447370814747360dbece3800afa6d75a1f485
Franko961 <franko961@hotmail.com>
2026-07-14 22:28:19 +0200

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -25,6 +25,7 @@ from routers.email import router as email_router
 from routers.admin import router as admin_router
 from routers.subscription import router as subscription_router
 from routers.integrations import router as integrations_router
+from routers.demo_requests import router as demo_requests_router
 from services.startup_service import run_startup, run_shutdown
 from core.exceptions import AppError
 from core.config import CORS_ORIGINS
@@ -68,6 +69,7 @@ app.include_router(email_router)
 app.include_router(admin_router)
 app.include_router(subscription_router)
 app.include_router(integrations_router)
+app.include_router(demo_requests_router)
 
 app.add_middleware(
     CORSMiddleware,
