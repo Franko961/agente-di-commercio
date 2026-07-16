0e61b1f339e9e313bd41d0769b0c282883c1ff98
Franko961 <franko961@hotmail.com>
2026-07-16 23:15:56 +0200

    Update order_repository.py

--- backend/repositories/order_repository.py
+++ backend/repositories/order_repository.py
@@ -10,6 +10,9 @@ class OrderRepository:
     async def find_by_client(self, user_id: str, client_id: str) -> list:
         return await self.collection.find({"user_id": user_id, "client_id": client_id}, {"_id": 0}).to_list(2000)
 
+    async def find_by_source_offer(self, offer_id: str, user_id: str):
+        return await self.collection.find_one({"source_offer_id": offer_id, "user_id": user_id}, {"_id": 0})
+
     async def find_one(self, oid: str, user_id: str):
         return await self.collection.find_one({"id": oid, "user_id": user_id}, {"_id": 0})
 
