842995294f6e51142c69e31823609d4e5d54fd66
Franko961 <franko961@hotmail.com>
2026-07-16 22:23:44 +0200

    Create order_repository.py

---
+++ backend/repositories/order_repository.py
@@ -0,0 +1,25 @@
+from core.database import db
+
+
+class OrderRepository:
+    collection = db.orders
+
+    async def find_many(self, user_id: str) -> list:
+        return await self.collection.find({"user_id": user_id}, {"_id": 0}).to_list(2000)
+
+    async def find_by_client(self, user_id: str, client_id: str) -> list:
+        return await self.collection.find({"user_id": user_id, "client_id": client_id}, {"_id": 0}).to_list(2000)
+
+    async def find_one(self, oid: str, user_id: str):
+        return await self.collection.find_one({"id": oid, "user_id": user_id}, {"_id": 0})
+
+    async def insert(self, doc: dict) -> dict:
+        await self.collection.insert_one(doc)
+        doc.pop("_id", None)
+        return doc
+
+    async def delete(self, oid: str, user_id: str) -> None:
+        await self.collection.delete_one({"id": oid, "user_id": user_id})
+
+
+order_repository = OrderRepository()
