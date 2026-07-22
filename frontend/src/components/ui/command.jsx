7bdd45b42047286c491e8b9e3f2bee6bde15d475
Franko961 <franko961@hotmail.com>
2026-07-22 21:50:00 +0200

    Update command.jsx

--- frontend/src/components/ui/command.jsx
+++ frontend/src/components/ui/command.jsx
@@ -83,7 +83,7 @@ const CommandItem = React.forwardRef(({ className, ...props }, ref) => (
   <CommandPrimitive.Item
     ref={ref}
     className={cn(
-      "relative flex cursor-default gap-2 select-none items-center rounded-sm px-2 py-1.5 text-sm outline-none data-[disabled=true]:pointer-events-none data-[selected=true]:bg-accent data-[selected=true]:text-accent-foreground data-[disabled=true]:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0",
+      "relative flex cursor-default gap-2 select-none items-center rounded-sm px-2 py-1.5 text-sm outline-none scale-100 origin-left transition-transform data-[disabled=true]:pointer-events-none data-[selected=true]:scale-[1.04] data-[selected=true]:bg-accent data-[selected=true]:text-accent-foreground data-[selected=true]:font-semibold data-[disabled=true]:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0",
       className
     )}
     {...props} />
