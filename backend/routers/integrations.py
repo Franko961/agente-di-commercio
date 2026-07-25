caa2be90986ef3c94402f8b813e22e2d03c389b2
Franko961 <franko961@hotmail.com>
2026-07-25 23:59:44 +0200

    Update integrations.py

--- backend/routers/integrations.py
+++ backend/routers/integrations.py
@@ -1,5 +1,5 @@
 import logging
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends
 from fastapi.responses import RedirectResponse
 
 from core.security import get_current_user
